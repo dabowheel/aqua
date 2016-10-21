4211461c906bd8fed922bac3f0b68b47cb32b7eb
Miguel Provencio <miguel.a.provencio@protonmail.com>
2016-10-21 08:54:57 -0600

    declare node function

--- include/aqua/node.h
+++ include/aqua/node.h
@@ -1,4 +1,13 @@
+#include "win32.h"
+#include "string.h"
+
 typedef struct a_node {
     void *data;
     struct a_node *next;
 } *a_node;
+
+EXPORT a_node a_nodeCreate(void *data);
+EXPORT a_node a_nodePush(a_node head, a_node node);
+EXPORT a_node *a_hashCreate(size_t array_size);
+EXPORT int a_hash(a_string key, size_t array_size);
+EXPORT void a_hashAdd(a_node *array, a_node item, a_string key, size_t array_size);
--- src/node.c
+++ src/node.c
@@ -1,22 +1,22 @@
 #include <string.h>
 #include <aqua/node.h>
 #include <aqua/util.h>
-#include <aqua/string.h>
+#include <aqua/win32.h>
 
-a_node a_nodeCreate(void *data)
+EXPORT a_node a_nodeCreate(void *data)
 {
     a_node node = a_malloc(sizeof(struct a_node));
     node->data = data;
     return node;
 }
 
-a_node a_nodePush(a_node head, a_node node)
+EXPORT a_node a_nodePush(a_node head, a_node node)
 {
     node->next = head;
     return node;
 }
 
-a_node *a_hashCreate(size_t array_size)
+EXPORT a_node *a_hashCreate(size_t array_size)
 {
     a_node *array;
     array = a_malloc(sizeof(a_node) * array_size);
@@ -24,7 +24,7 @@ a_node *a_hashCreate(size_t array_size)
     return array;
 }
 
-int a_hash(a_string key, size_t array_size)
+EXPORT int a_hash(a_string key, size_t array_size)
 {
     int value = 0;
     for (int i = 0; i < 10 && i < key->len; i++) {
@@ -34,7 +34,7 @@ int a_hash(a_string key, size_t array_size)
     return value;
 }
 
-void a_hashAdd(a_node *array, a_node item, a_string key, size_t array_size)
+EXPORT void a_hashAdd(a_node *array, a_node item, a_string key, size_t array_size)
 {
     int hash = a_hash(key, array_size);
     array[hash] = a_nodePush(array[hash], item);
